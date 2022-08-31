e7593204e4edd7d8acc6ea893f18339ea3869618
anand <anandandsahoo@gmail.com>
2022-09-01 00:54:58 +0530

    deletion of element at the end in LL

---
+++ dsa/deletionatendLL.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void traversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+}
+struct node *deletionatend(struct node *head)
+{
+    struct node*p=head;
+    struct node *q = head->next;
+    while (q->next!=NULL)
+    {
+        p=p->next;
+        q=q->next;
+    }
+    p->next=NULL;
+    free(q);
+    return head;
+}
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+    head->data = 9;
+    head->next = second;
+    second->data = 10;
+    second->next = third;
+    third->data = 15;
+    third->next = fourth;
+    fourth->data = 18;
+    fourth->next = NULL;
+
+    traversal(head);
+    deletionatend(head);
+    printf("\n");
+    traversal(head);
+
+    return 0;
+}
