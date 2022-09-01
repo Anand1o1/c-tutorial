a967251a6b4c107108f68e495131efc501acb49c
anand <anandandsahoo@gmail.com>
2022-09-01 11:33:16 +0530

    deletion of LL with a given value

---
+++ dsa/deletionwithagivenvalue.cc
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void traversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+};
+struct node *deletionwithvalue(struct node *head, int value)
+{
+    struct node *p = head;
+    struct node *q = head->next;
+    while (q->data != value && q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    if (q->data == value)
+    {
+        p->next = q->next;
+        free(q);
+    
+    }
+    return head;
+};
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+    head->data = 9;
+    head->next = second;
+    second->data = 10;
+    second->next = third;
+    third->data = 15;
+    third->next = fourth;
+    fourth->data = 18;
+    fourth->next = NULL;
+
+    traversal(head);
+    deletionwithvalue(head, 15);
+    printf("\n");
+    traversal(head);
+
+    return 0;
+}
